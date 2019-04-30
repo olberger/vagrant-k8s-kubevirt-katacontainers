88179f48c5daeacfd8ce35f2980df13afeef31af
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2019-04-30 18:45:49 +0200

    Activate DataVolumes feature gate

--- kubevirt.sh
+++ kubevirt.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+kubectl create namespace kubevirt
+
+echo "Activate DataVolumes feature gate"
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+data:
+  feature-gates: DataVolumes
+kind: ConfigMap
+metadata:
+  name: kubevirt-config
+  namespace: kubevirt
+EOF
+
 echo "Deploy kubevirt"
 export KUBEVIRT_RELEASE=$(curl --silent "https://api.github.com/repos/kubevirt/kubevirt/releases/latest" | grep '"tag_name":'| sed -E 's/.*"([^"]+)".*/\1/')
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_RELEASE/kubevirt-operator.yaml
